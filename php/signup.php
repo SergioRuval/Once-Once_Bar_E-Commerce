d1e9a640783c04c157991db493b5495183bddd31
SergioRuval <ruvaloser@gmail.com>
2020-12-21 15:23:00 -0600

    Inicio de la implementación de sesiones, se necesita hacer commit y fusionar con la rama principal local para ver los cambios bien actualizados en el servidor local

--- php/signup.php
+++ php/signup.php
@@ -1,6 +1,20 @@
 <?php
-    $nombre = $cuenta = $contraseña = $email = "";
+    $nombre = $cuenta = $password = $email = "";
 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nombre = $_POST["nombre"];
+        $cuenta = $_POST["correo"];
+        $password = $_POST["login"];
+        $email = $_POST["contra"];
+
+        session_start();
+
+        $_SESSION["CUENTA"] = $cuenta;
+        $_SESSION["CARRITO"] = "";
+        $_SESSION["EMAIL"] = $email;
+
+        header("Location: ../index.php");
+    }
     
 
 ?>
