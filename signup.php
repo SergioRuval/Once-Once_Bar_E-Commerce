1e4e44fb0ed2608c37fbd37f3547fa8e0426c867
SergioRuval <ruvaloser@gmail.com>
2020-12-22 21:24:37 -0600

    Se agregaron los favicon a todas las páginas

--- signup.php
+++ signup.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel='icon' href='../images/favicon.ico' type='image/x-icon'>
+    <link rel='icon' href='images/favicon.ico' type='image/x-icon'>
     <title>Regístrarse</title>
 
     <style>
